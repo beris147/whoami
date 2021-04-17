c113a4a3e6800655d85f73e535af4dcc26d28f16
beris147 <edwir147@gmail.com>
2021-04-17 13:39:05 -0500

    SocketContext works

--- whoami-client/src/components/CreateRoom.js
+++ whoami-client/src/components/CreateRoom.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import SocketContext from '../contexts/SocketContext';
 
 import './CreateRoom.css';
 
 function CreateRoom() {
+  const socket = useContext(SocketContext);
+  useEffect(() => {
+    const data = { username: 'Edgar' };
+    
+    socket.emit('create-room', data, error => {
+      if(error) {
+        console.log(error);
+      } 
+    });
+
+    socket.on('joined-room', data => {
+      console.log(data);
+    });
+
+    socket.on('room-update', data => {
+      console.log('room updated + ', data);
+    });
+    
+  }, []);
+
   return <h1>Create Room</h1>;
 }
 
