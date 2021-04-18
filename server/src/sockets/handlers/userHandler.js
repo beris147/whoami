4f7246ed7cae5286274f95e5b5c16b8c05118dd9
beris147 <edwir147@gmail.com>
2021-04-18 14:23:11 -0500

    TODO check if user was the owner

--- server/src/sockets/handlers/userHandler.js
+++ server/src/sockets/handlers/userHandler.js
@@ -23,6 +23,11 @@ module.exports = (
           removeRoom(rooms[user.roomId], rooms);
         }
         else emitRoomUpdate(rooms[user.roomId], io);
+        /* 
+        TODO
+        Check if the user that left was the owner, if true we need to change the
+        current owner. Implement this when we can test if it works. 
+        */
       } catch (error) {
         console.log(error);
       }
