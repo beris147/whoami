7651750a6057e522ebf98c4d56671f785e817ab0
beris147 <edwir147@gmail.com>
2021-04-19 14:25:53 -0500

    errorCallBack moved to be reused later

--- client/src/components/CreateRoom.js
+++ client/src/components/CreateRoom.js
@@ -2,24 +2,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import SocketContext from '../contexts/SocketContext';
+import errorCallBack from '../utils/errorCallBack'
 
-import type { 
-  CreateRoomT, 
-  ErrorCallBackT, 
-  ErrorT,
-  RoomT,
-} from 'common/types';
+import type { CreateRoomT, RoomT } from 'common/types';
 
 function CreateRoom(): React$Element<any> {
   const history: any = useHistory();
   const socket: any = useContext(SocketContext);
   const [username: string, setUsername: mixed] = useState('');
-
-  const errorCallBack: ErrorCallBackT = (error: ?ErrorT): void => {
-    if(error) {
-      console.log(error);
-    }
-  }
 
   const handleCreateRoom: mixed = (): void => {
     const data: CreateRoomT = { username };
@@ -29,9 +19,6 @@ function CreateRoom(): React$Element<any> {
   useEffect((): void => {
     socket.on('joined-room', (data: RoomT): void => {
       history.push(`/room/${data.id}`);
-    });
-    socket.on('error', (error: ErrorT) => {
-      console.log(error);
     });
   }, [socket, history]);
 
---
+++ client/src/utils/errorCallBack.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react';
+
+import type { ErrorCallBackT, ErrorT } from 'common/types';
+
+const errorCallBack: ErrorCallBackT = (error: ?ErrorT): void => {
+  if(error) {
+    console.log(error);
+  }
+}
+
+export default errorCallBack;
