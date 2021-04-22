0ccc463f8801a92517987d624168b68f971ef848
GustavoMeza <coollive11@gmail.com>
2021-04-22 12:46:26 -0500

    Tests for getting a user room

---
+++ server/src/utils/__tests__/userUtils/getUserRoom.test.js
@@ -0,0 +1,42 @@
+// @flow
+const { describe, it, expect } = require("@jest/globals");
+const { getUserRoom } = require("utils/userUtils");
+
+import type { RoomT, RoomSetT, UserT, UserSetT } from "common/types";
+
+describe("getUserRoom", () => {
+  describe("Given a user whose room appears in the room set", () => {
+    it("Returns the room", () => {
+      const roomId="room-id";
+      const user : UserT = {
+        id: "user-id",
+        roomId: roomId,
+        username: "user-username",
+      };
+      const expectedRoom : RoomT = {
+          id: roomId,
+          owner: "owner-id",
+          round: 1,
+          time: 1,
+          users: ["username-1", "username-2"],
+      };
+      const rooms : RoomSetT = {
+          [roomId] : expectedRoom,
+      };
+      const room = getUserRoom(user, rooms);
+      expect(room).toStrictEqual(expectedRoom);
+    });
+  });
+  describe("Given a user whose room doesn't appear in the room set", () => {
+    it("Returns undefined", () => {
+      const user : UserT = {
+        id: "user-id",
+        roomId: "room-id",
+        username: "user-username",
+      };
+      const rooms : RoomSetT = {};
+      const room = getUserRoom(user, rooms);
+      expect(room).toBeUndefined();
+    });
+  });
+});
