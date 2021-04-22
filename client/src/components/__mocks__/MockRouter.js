049ddd66759cf3dae1e432377ce9dd3225fe9d37
beris147 <edwir147@gmail.com>
2021-04-22 18:07:08 -0500

    room tests

--- client/src/components/__mocks__/MockRouter.js
+++ client/src/components/__mocks__/MockRouter.js
@@ -1,14 +1,24 @@
 // @flow
 import React from 'react';
-import { MemoryRouter, Route } from 'react-router-dom';
+import { MemoryRouter, Route, Router } from 'react-router-dom';
 
 type MockRouterT = {
   ui: React$Element<any>,
-  initialEntries: [string],
+  initialEntries?: [string],
   path: string,
+  history?: any,
 };
 
 function MockRouter(props: MockRouterT): React$Element<any>{
+  if(props.history) {
+    return (
+      <Router history={props.history}>
+        <Route path={props.path}>
+          {props.ui}
+        </Route>
+      </Router>
+    );
+  }
   return (
     <MemoryRouter initialEntries={props.initialEntries}>
       <Route path={props.path}>
