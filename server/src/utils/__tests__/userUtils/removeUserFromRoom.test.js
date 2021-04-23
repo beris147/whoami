02964f9e26728eeac37e569766acd0501715a593
GustavoMeza <coollive11@gmail.com>
2021-04-22 22:00:12 -0500

    Code quality: DRY

--- server/src/utils/__tests__/userUtils/removeUserFromRoom.test.js
+++ server/src/utils/__tests__/userUtils/removeUserFromRoom.test.js
@@ -4,51 +4,40 @@ const { removeUserFromRoom } = require("utils/userUtils");
 
 import type { RoomT, RoomSetT, UserT, UserSetT, ErrorT } from "common/types";
 
+const user: UserT = {
+  id: "user-id",
+  username: "user-username",
+  roomId: "room-id",
+};
+
+const roomTemplate: RoomT = {
+  id: user.roomId,
+  owner: "owner-id",
+  round: 1,
+  time: 1,
+  users: ["username-1", "username-2", "username-3"],
+};
+
 describe("When removing a user from a room", () => {
   describe("Given a user whose room exists and their username appears in the room's list", () => {
     it("Removes the username from the room user list", () => {
-      const user: UserT = {
-        id: "user-id",
-        username: "user-username",
-        roomId: "room-id",
-      };
-      const userList: Array<string> = [
-        "username-1",
-        "username-2",
-        "username-3",
-      ];
       const room: RoomT = {
-        id: user.roomId,
-        owner: "owner-id",
-        round: 1,
-        time: 1,
-        users: [...userList, user.username],
-      };
-      const expectedRoom: RoomT = {
-        ...room,
-        users: userList,
+        ...roomTemplate,
+        users: [...roomTemplate.users, user.username],
       };
       const rooms: RoomSetT = {
-        [room.id]: room
+        [room.id]: room,
       };
 
       removeUserFromRoom(user, rooms);
-      expect(rooms[user.roomId]).toStrictEqual(expectedRoom);
+      expect(rooms[user.roomId]).toStrictEqual(roomTemplate);
     });
   });
   describe("Given a user whose room exists and their username is the only one in the room's list", () => {
     it("Keeps the room", () => {
       // TODO: removing the only user left in the room should delete the room
-      const user: UserT = {
-        id: "user-id",
-        username: "user-username",
-        roomId: "room-id",
-      };
       const room: RoomT = {
-        id: user.roomId,
-        owner: "owner-id",
-        round: 1,
-        time: 1,
+        ...roomTemplate,
         users: [user.username],
       };
       const rooms: RoomSetT = {
@@ -61,17 +50,9 @@ describe("When removing a user from a room", () => {
   describe("Given a user whose room exists and owns it", () => {
     it("Does nothing", () => {
       // TODO: removing the owner should change ownership
-      const user: UserT = {
-        id: "user-id",
-        username: "user-username",
-        roomId: "room-id",
-      };
       const room: RoomT = {
-        id: user.roomId,
+        ...roomTemplate,
         owner: user.username,
-        round: 1,
-        time: 1,
-        users: ["username-1", "username-2", "username-3"],
       };
       const rooms: RoomSetT = {
         [room.id]: room,
@@ -82,23 +63,7 @@ describe("When removing a user from a room", () => {
   });
   describe("Given a user whose room exists and their username doesn't appear in the room's list", () => {
     it("Does nothing", () => {
-      const user: UserT = {
-        id: "user-id",
-        username: "user-username",
-        roomId: "room-id",
-      };
-      const userList: Array<string> = [
-        "username-1",
-        "username-2",
-        "username-3",
-      ];
-      const room: RoomT = {
-        id: user.roomId,
-        owner: "owner-id",
-        round: 1,
-        time: 1,
-        users: userList,
-      };
+      const room: RoomT = roomTemplate;
       const rooms: RoomSetT = {
         [room.id]: room,
       };
@@ -109,11 +74,6 @@ describe("When removing a user from a room", () => {
   });
   describe("Given a user whose room doesn't exists", () => {
     it("Throws an error", () => {
-      const user: UserT = {
-        id: "user-id",
-        username: "user-username",
-        roomId: "room-id",
-      };
       const rooms: RoomSetT = {};
       expect(() => {
         removeUserFromRoom(user, rooms);
